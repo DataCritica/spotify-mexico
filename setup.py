3374aebaf6932f7927933dfbfe256074c82e40a5
Fer Aguirre <kay_op@protonmail.com>
2022-03-23 21:01:52 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Spotify Mexico',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Analysis of weekly top songs in Spotify Mexico from 2020-12-01 to 2022-02-17',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
